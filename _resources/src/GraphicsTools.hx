e6c0b312d830dfb97bb68e2a8b2475869ddc0f4c
Will Blanton <will@Wills-MBP.fios-router.home>
2019-12-19 22:01:12 -0500

    add line drawing, poly drawing

--- _resources/src/GraphicsTools.hx
+++ _resources/src/GraphicsTools.hx
@@ -1,4 +1,5 @@
 import zero.utilities.Color;
+import zero.utilities.Vec2;
 import pixi.core.graphics.Graphics;
 
 class GraphicsTools {
@@ -14,6 +15,17 @@ class GraphicsTools {
 		graphic.endFill();
 		return graphic;
 	}
+	public static function fill_poly(graphic:Graphics, color:Color, path:Array<Vec2>):Graphics {
+		graphic.beginFill(color.to_hex_24(), color.alpha);
+		var _path = [];
+		for (v in path) {
+			_path.push(v.x);
+			_path.push(v.y);
+		}
+		graphic.drawPolygon(_path);
+		graphic.endFill();
+		return graphic;
+	}
 	public static function circle(graphic:Graphics, color:Color, x:Float, y:Float, radius:Float, line_width:Float = 1, alignment:LineAlign = CENTER):Graphics {
 		graphic.lineStyle(line_width, color.to_hex_24(), color.alpha, cast alignment);
 		graphic.drawCircle(x, y, radius);
@@ -23,6 +35,24 @@ class GraphicsTools {
 	public static function rect(graphic:Graphics, color:Color, x:Float, y:Float, width:Float, height:Float, radius:Float = 0, line_width:Float = 1, alignment:LineAlign = CENTER):Graphics {
 		graphic.lineStyle(line_width, color.to_hex_24(), color.alpha, cast alignment);
 		graphic.drawCircle(x, y, radius);
+		graphic.lineStyle();
+		return graphic;
+	}
+	public static function poly(graphic:Graphics, color:Color, path:Array<Vec2>,line_width:Float = 1, alignment:LineAlign = CENTER):Graphics {
+		graphic.lineStyle(line_width, color.to_hex_24(), color.alpha, cast alignment);
+		var _path = [];
+		for (v in path) {
+			_path.push(v.x);
+			_path.push(v.y);
+		}
+		graphic.drawPolygon(_path);
+		graphic.lineStyle();
+		return graphic;
+	}
+	public static function line(graphic:Graphics, color:Color, p0x:Float, p0y:Float, p1x:Float, p1y:Float, thickness:Float = 1, alignment:LineAlign = CENTER) {
+		graphic.lineStyle(thickness, color.to_hex_24(), color.alpha, cast alignment);
+		graphic.moveTo(p0x, p0y);
+		graphic.lineTo(p1x, p1y);
 		graphic.lineStyle();
 		return graphic;
 	}
